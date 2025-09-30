9f44f9f4cfa95b87a12343afd23276604e0a0b36
Alfons Wallin <144wallfin@gmail.com>
2025-09-30 16:02:02 +0200

    Altered some icons, and started working on fontchanges.

--- Assets/Scripts/TimeHandler/TimeMeterScriptedAnimations/ITimeAnimation.cs
+++ Assets/Scripts/TimeHandler/TimeMeterScriptedAnimations/ITimeAnimation.cs
@@ -4,5 +4,4 @@ using UnityEngine.UI;
 public interface ITimeAnimation
 {
     void UpdateAnimation(Image image, float time, float maxTime);
-    //void UpdateFillAmount(Image image, float time, float maxTime);
 }
--- Assets/Scripts/TimeHandler/TimeMeterScriptedAnimations/StepTimeAnimation.cs
+++ Assets/Scripts/TimeHandler/TimeMeterScriptedAnimations/StepTimeAnimation.cs
@@ -21,19 +21,4 @@ public class StepTimeAnimation : MonoBehaviour, ITimeAnimation
         }
         image.fillAmount = fillAmounts[^1];
     }
-
-    /* public void UpdateFillAmount(Image images, float time, float maxTime)
-    {
-        float[] thresholds = { 0.75f, 0.5f, 0.25f, 0f };
-        float[] fillAmounts = { 1f, 0.75f, 0.5f, 0.25f, 0f };
-
-        for (int i = images.Length; i > 0 ; i--)
-        {
-            if (time > maxTime * thresholds[i - 1])
-            {
-                images[i - 1].fillAmount = fillAmounts[i];
-                return;
-            }
-        }
-    } */
 }
